44f9b2521a52dfd4d00f7efd3b9eed47e9efd106
Gian Pinto <gianrobertpinto@gmail.com>
2024-10-28 21:17:12 -0600

    Initial Commit

---
+++ PhotoFrame/PhotoFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFrameApp.swift
+//  PhotoFrame
+//
+//  Created by Gian Pinto on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
